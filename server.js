f5bd8506f2d80f82d8940c0def18632108153d11
CalebAllard <allard.caleb@gmail.com>
2020-01-21 12:39:36 -0500

    mm

--- server.js
+++ server.js
@@ -5,5 +5,23 @@ const db = require('./data/dbConfig.js');
 const server = express();
 
 server.use(express.json());
+// CRUD
+server.get('/api/accounts',  (req, res,next) => {
+    
+        
+});
+server.get('/api/accounts/:id',  (req, res) => {
+    
+});
+server.post('/api/accounts',  (req, res) => {
+    
+    
+});
+server.put('/api/accounts/:id',  (req, res) => {
+    
+});
+server.delete('/api/accounts/:id',  (req, res) => {
+    
+});
 
 module.exports = server;
