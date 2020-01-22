d121aad06c6f5657dba0763d25ff9fe60eed6735
CalebAllard <allard.caleb@gmail.com>
2020-01-22 09:05:09 -0500

    finished crud with knex sql

--- server.js
+++ server.js
@@ -7,21 +7,67 @@ const server = express();
 server.use(express.json());
 // CRUD
 server.get('/api/accounts',  (req, res,next) => {
-    
+    db('accounts').select()
+    .then(ret => {
+        res.status(200).json(ret);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err: 'probelm getting accounts'});
+    });
         
 });
 server.get('/api/accounts/:id',  (req, res) => {
-    
+    db('accounts').where({id:req.params.id}).select()
+    .then(ret => {
+        res.status(200).json(ret);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err: 'problem getting account'});
+    });
 });
-server.post('/api/accounts',  (req, res) => {
-    
+server.post('/api/accounts', (req, res) => {
+    db('accounts').insert(req.body)
+    .then(ret => {
+        const id = ret[0];
+        return id;
+    })
+    .then(ret => {
+      db('accounts').where({id:ret}).select()
+      .then(ret =>{
+          res.status(201).json(ret);
+      })
+      .catch(err => {
+          res.status(500).json({msg: 'Created account, problem returning account'})
+      })
+     
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err: 'problem creating account'});
+    });
     
 });
 server.put('/api/accounts/:id',  (req, res) => {
-    
+    db('accounts').where({id:req.params.id}).update(req.body)
+    .then(ret =>{
+        res.status(201).json(ret);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({err:'problem updating account'});
+    })
 });
 server.delete('/api/accounts/:id',  (req, res) => {
-    
+    db('accounts').where({id:req.params.id}).delete()
+    .then(ret => {
+        res.status(200).json(ret);
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({err:'problem deleting account'});
+    });
 });
 
 module.exports = server;
